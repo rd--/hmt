79dd008c1ff2b01b41fbcdef00ea52016f952452
Rohan Drape <rd@slavepianos.org>
2007-04-13 05:34:26 +0000

    Fix error in Rahn prime form function

--- Music/Theory/Rahn.hs
+++ Music/Theory/Rahn.hs
@@ -7,6 +7,4 @@ prime :: (Integral a) => [a] -> [a]
 prime = P.prime cmp
 
 cmp :: (Ord t) => [t] -> [t] -> Ordering
-cmp [] [] = EQ
-cmp p  q  = if r == EQ then cmp p q else r
-    where r = compare (last p) (last q)
+cmp p q = compare (reverse p) (reverse q)
