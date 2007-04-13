9f0aeead8879f308051a8b1d2214628da7e88942
Rohan Drape <rd@slavepianos.org>
2007-04-13 09:40:50 +0000

    Move Forte table to separate module

--- Music/Theory.hs
+++ Music/Theory.hs
@@ -1,7 +1,9 @@
 module Music.Theory (module Music.Theory.Forte,
                      module Music.Theory.Pitch,
-                     module Music.Theory.Set) where
+                     module Music.Theory.Set,
+                     module Music.Theory.Table) where
 
 import Music.Theory.Forte
 import Music.Theory.Pitch
 import Music.Theory.Set
+import Music.Theory.Table
