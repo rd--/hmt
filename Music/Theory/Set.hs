cf9c481166f40bbb11897fdb4c336abc50e3aa3a
Rohan Drape <rd@slavepianos.org>
2007-04-13 09:41:33 +0000

    Implement Set.dyads

--- Music/Theory/Set.hs
+++ Music/Theory/Set.hs
@@ -1,4 +1,4 @@
-module Music.Theory.Set (powerset, permutations) where
+module Music.Theory.Set (powerset, permutations, dyads) where
 
 import Control.Monad
 
@@ -12,3 +12,7 @@ permutations (x:xs) = [ zs | ys <- permutations xs , zs <- everywhere x ys ]
 everywhere :: a -> [a] -> [[a]]
 everywhere x []     = [[x]]
 everywhere x (y:ys) = (x:y:ys) : [ y:zs | zs <- everywhere x ys ]
+
+dyads :: [a] -> [(a,a)]
+dyads [] = []
+dyads (x:xs) = dyads xs ++ [ (x,y) | y <- xs ]
