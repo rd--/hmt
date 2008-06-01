26c3e5988f7ee0ff5c53f2b6dd7b37de454882bc
Rohan Drape <rd@slavepianos.org>
2008-06-01 03:04:18 +0000

    rexport encode and rahn prime forms

--- Music/Theory.hs
+++ Music/Theory.hs
@@ -1,9 +1,13 @@
-module Music.Theory (module Music.Theory.Forte,
+module Music.Theory (module Music.Theory.Encode,
+                     module Music.Theory.Forte,
                      module Music.Theory.Pitch,
+                     module Music.Theory.Rahn,
                      module Music.Theory.Set,
                      module Music.Theory.Table) where
 
+import Music.Theory.Encode
 import Music.Theory.Forte
 import Music.Theory.Pitch
+import Music.Theory.Rahn
 import Music.Theory.Set
 import Music.Theory.Table
