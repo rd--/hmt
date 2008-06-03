a70f81dbf9f2471de2261d1a6e9c169607a7db00
Rohan Drape <rd@slavepianos.org>
2008-06-03 12:48:26 +0000

    haddock comments

--- Music/Theory/Prime.hs
+++ Music/Theory/Prime.hs
@@ -3,6 +3,7 @@ module Music.Theory.Prime (prime) where
 import Data.List
 import Music.Theory.Pitch
 
+-- | Prime form rule requiring comparator.
 prime :: (Integral a) => ([a] -> [a] -> Ordering) -> [a] -> [a]
 prime _ [] = []
 prime f p = minimumBy f (map (transposeTo 0) r)
--- Music/Theory/Set.hs
+++ Music/Theory/Set.hs
@@ -2,17 +2,20 @@ module Music.Theory.Set (powerset, permutations, dyads) where
 
 import Control.Monad
 
+-- | Powerset, ie. set of all all subsets.
 powerset :: [a] -> [[a]]
 powerset = filterM (const [True, False])
 
+-- | Permutations.
 permutations :: [a] -> [[a]]
-permutations []     = [[]]
+permutations [] = [[]]
 permutations (x:xs) = [ zs | ys <- permutations xs , zs <- everywhere x ys ]
 
 everywhere :: a -> [a] -> [[a]]
 everywhere x []     = [[x]]
 everywhere x (y:ys) = (x:y:ys) : [ y:zs | zs <- everywhere x ys ]
 
+-- | Two element subsets (cf [2] . powerset).
 dyads :: [a] -> [(a,a)]
 dyads [] = []
 dyads (x:xs) = dyads xs ++ [ (x,y) | y <- xs ]
