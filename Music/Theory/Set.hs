61beb1c5001a534d8f2459fc7c163d4fcd76daae
Rohan Drape <rd@slavepianos.org>
2008-06-03 23:47:55 +0000

    implement se (set expansion)

--- Music/Theory/Set.hs
+++ Music/Theory/Set.hs
@@ -1,6 +1,7 @@
-module Music.Theory.Set (powerset, permutations, dyads) where
+module Music.Theory.Set where
 
 import Control.Monad
+import Data.List
 
 -- | Powerset, ie. set of all all subsets.
 powerset :: [a] -> [[a]]
@@ -10,12 +11,16 @@ powerset = filterM (const [True, False])
 permutations :: [a] -> [[a]]
 permutations [] = [[]]
 permutations (x:xs) = [ zs | ys <- permutations xs , zs <- everywhere x ys ]
-
-everywhere :: a -> [a] -> [[a]]
-everywhere x []     = [[x]]
-everywhere x (y:ys) = (x:y:ys) : [ y:zs | zs <- everywhere x ys ]
+    where everywhere x [] = [[x]]
+          everywhere x (y:ys) = (x:y:ys) : [ y:zs | zs <- everywhere x ys ]
 
 -- | Two element subsets (cf [2] . powerset).
 dyads :: [a] -> [(a,a)]
 dyads [] = []
 dyads (x:xs) = dyads xs ++ [ (x,y) | y <- xs ]
+
+-- | Set expansion
+se :: (Ord a) => Int -> [a] -> [[a]]
+se n xs = if length xs == n 
+          then [xs] 
+          else nub (concatMap (se n) [sort (y : xs) | y <- xs])
