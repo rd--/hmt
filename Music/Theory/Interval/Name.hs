b9875e43ac6492d2417511500435242baa554d9e
Rohan Drape <rd@slavepianos.org>
2011-03-10 02:09:21 +0000

    Music.Theory.Interval.Name

---
+++ Music/Theory/Interval/Name.hs
@@ -0,0 +1,7 @@
+module Music.Theory.Interval.Name where
+
+import Music.Theory.Interval
+
+perfect_fifth,major_seventh :: Interval
+perfect_fifth = Interval Fifth Perfect GT 0
+major_seventh = Interval Seventh Major GT 0
