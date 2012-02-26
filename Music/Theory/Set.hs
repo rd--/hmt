e46f1e5ba080834eb895fd6aa50c14ad0949ba05
Rohan Drape <rd@slavepianos.org>
2012-02-26 09:35:57 +0000

    +partitions

--- Music/Theory/Set.hs
+++ Music/Theory/Set.hs
@@ -3,6 +3,7 @@ module Music.Theory.Set where
 
 import Control.Monad
 import Data.List
+import qualified Math.Combinatorics.Multiset as M {- multiset-comb -}
 
 -- | Remove duplicate elements with 'nub' and then 'sort'.
 --
@@ -30,6 +31,16 @@ dyads s =
 -- > se 4 [1,2,3] == [[1,1,2,3],[1,2,2,3],[1,2,3,3]]
 se :: (Ord a) => Int -> [a] -> [[a]]
 se n xs =
-    if length xs == n
+    if length xs >= n
     then [xs]
     else nub (concatMap (se n) [sort (y : xs) | y <- xs])
+
+-- | All distinct multiset partitions, see 'M.partitions'.
+--
+-- > partitions "aab" == [["aab"],["a","ab"],["b","aa"],["b","a","a"]]
+--
+-- > partitions "abc" == [["abc"]
+-- >                     ,["bc","a"],["b","ac"]
+-- >                     ,["c","ab"],["c","b","a"]]
+partitions :: Eq a => [a] -> [[[a]]]
+partitions = map (map M.toList . M.toList) . M.partitions . M.fromListEq
