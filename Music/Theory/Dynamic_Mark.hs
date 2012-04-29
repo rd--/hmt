e1a4874daeb73accdf4f071b311894525b5c04b2
Rohan Drape <rd@slavepianos.org>
2012-04-29 05:15:07 +0000

    +Niente

--- Music/Theory/Dynamic_Mark.hs
+++ Music/Theory/Dynamic_Mark.hs
@@ -1,7 +1,8 @@
 -- | Common music notation dynamic marks.
 module Music.Theory.Dynamic_Mark where
 
-data Dynamic_Mark_T = PPPPP | PPPP | PPP | PP | P | MP
+data Dynamic_Mark_T = Niente
+                    | PPPPP | PPPP | PPP | PP | P | MP
                     | MF | F | FF | FFF | FFFF | FFFFF
                     | FP | SF | SFP | SFPP | SFZ | SFFZ
                       deriving (Eq,Ord,Enum,Bounded,Show)
