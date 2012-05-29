c2262608a24ea38eaf6c83c74d9a1a79914b2d7c
Rohan Drape <rd@slavepianos.org>
2012-05-29 01:46:39 +0000

    ~Tiling/Johnson_2009,-E&C,+R

--- Music/Theory/Tiling/Johnson_2009.hs
+++ Music/Theory/Tiling/Johnson_2009.hs
@@ -5,64 +5,84 @@ module Music.Theory.Tiling.Johnson_2009 where
 import Data.List
 import Data.List.Split {- split -}
 
-type E = ([Int],[(Int,[Int])])
-type C = [E]
-type V = [[Int]]
+-- | Canon of /(period,sequence,multipliers,displacements)/.
+type R = (Int,[Int],[Int],[Int])
 
-e_voices :: E -> V
-e_voices (p,q) =
-    let f (m,n) = let p' = map (* m) p
-                  in map (\i -> map (+ i) p') n
-    in return (sort (concat (concatMap f q)))
+-- | Voice.
+type V = [Int]
 
-c_voices :: C -> V
-c_voices = concatMap e_voices
-
-v_limit :: V -> Int
-v_limit = maximum . concat
-
-v_dot_star :: V -> [String]
-v_dot_star v =
-    let n = v_limit v
-        f p i = if i `elem` p then '*' else '.'
-        g p = map (f p) [0..n]
-    in map g v
-
-v_dot_star_m :: Int -> V -> [String]
-v_dot_star_m m =
-    let f = concat . intersperse "|" . splitEvery m
-    in map f . v_dot_star
-
-e_no_mul :: ([Int],[Int]) -> [E]
-e_no_mul (p,q) =
-    let q' = zip (repeat 1) (map return q)
-    in map (\i -> (p,[i])) q'
-
-c_no_mul :: [([Int],[Int])] -> C
-c_no_mul = concat . map e_no_mul
-
--- | Print @.*@ diagram for 'C'.
+-- | Cycle at /period/.
 --
--- > c_print (c_no_mul [([0,2,5],[0,1,8]),([0,3,5],[4,11,12])])
--- > c_print (c_no_mul [([0..5],[0,6,12,18])])
--- > c_print (c_no_mul [([0,2,3,4,5,7],[0,6,12,18])])
--- > c_print (c_no_mul [([0,1,3,4,5,8],[0,6,12,18])])
--- > c_print (c_no_mul [([0,3,4,5,7,8],[0,6,12,18])])
-c_print :: C -> IO ()
-c_print = putStrLn . unlines . ("" :) . v_dot_star . c_voices
+-- > take 9 (p_cycle 18 [0,2,5]) == [0,2,5,18,20,23,36,38,41]
+p_cycle :: Int -> [Int] -> [Int]
+p_cycle n s = s ++ p_cycle n (map (+ n) s)
+
+-- | Set of 'V' from 'R'.
+--
+-- > v_print 24 (r_voices (12,[0..5],[1,1],[0,6]))
+-- > v_print 24 (r_voices (12,[0,2,3,4,5,7],[1,1],[0,6]))
+-- > v_print 24 (r_voices (12,[0,1,3,4,5,8],[1,1],[0,6]))
+-- > v_print 24 (r_voices (12,[0,3,4,5,7,8],[1,1],[0,6]))
+--
+-- > let r = (36,[0,1,3],[1,2,3,4,5,6,7,8],[0,7,25,51,52,78,105,130])
+-- > in v_print_m_from 18 6 6 (r_voices r)
+r_voices :: R -> [V]
+r_voices (p,s,m,o) =
+    let f (i,j) = p_cycle p (map ((+ j) . (* i)) s)
+    in map f (zip m o)
+
+-- | 'concatMap' of 'r_voices'.
+--
+-- > v_print 36 (rr_voices [(18,[0,2,5],[1,1,1],[0,1,8])
+-- >                       ,(18,[0,3,5],[1,1,1],[4,11,12])])
+rr_voices :: [R] -> [V]
+rr_voices = concatMap r_voices
+
+-- | Variant of 'elem' for ordered sequences, which can therefore
+-- return 'False' when searching infinite sequences.
+--
+-- > 5 `elemOrd` [0,2..] == False
+-- > 10 `elemOrd` [0,2..] == True
+elemOrd :: Ord a => a -> [a] -> Bool
+elemOrd i p =
+    case p of
+      [] -> False
+      j:p' -> case compare j i of
+                LT -> elemOrd i p'
+                EQ -> True
+                GT -> False
+
+-- | A @.*@ diagram of /n/ places of 'V'.
+--
+-- > v_dot_star 18 [0,2..] == "*.*.*.*.*.*.*.*.*."
+v_dot_star :: Int -> V -> String
+v_dot_star n v =
+    let f p i = if i `elemOrd` p then '*' else '.'
+    in map (f v) [0..n-1]
+
+-- | Insert @|@ every /n/ places.
+--
+-- > with_bars 6 (v_dot_star 18 [0,2..]) == "*.*.*.|*.*.*.|*.*.*."
+with_bars :: Int -> String -> String
+with_bars m = concat . intersperse "|" . splitEvery m
+
+-- | Variant with measure length /m/ and number of measures /n/.
+--
+-- > v_dot_star_m 6 3 [0,2..] == "*.*.*.|*.*.*.|*.*.*."
+v_dot_star_m :: Int -> Int -> V -> String
+v_dot_star_m m n = with_bars m . v_dot_star (n * m)
+
+-- | Print @.*@ diagram.
+v_print :: Int -> [V] -> IO ()
+v_print n = putStrLn . unlines . ("" :) . map (v_dot_star n)
 
 -- | Variant to print @|@ at measures.
---
--- > c_print_m (Just 6) (map (\i -> ([0,1,3],[i]))
--- >                         [(1,[0,36..180])
--- >                         ,(2,[7,7+36..180])
--- >                         ,(3,[25,25+36..180])
--- >                         ,(4,[51,51+36..180])
--- >                         ,(5,[52,52+36..180])
--- >                         ,(6,[78,78+36..180])
--- >                         ,(7,[105,105+36..180])
--- >                         ,(8,[130,130+36..180])])
-c_print_m :: Maybe Int -> C -> IO ()
-c_print_m n =
-    let f = maybe v_dot_star v_dot_star_m n
-    in putStrLn . unlines . ("":) . f . c_voices
+v_print_m :: Int -> Int -> [V] -> IO ()
+v_print_m m n = putStrLn . unlines . ("" :) . map (v_dot_star_m m n)
+
+-- | Variant that discards first /k/ measures.
+v_print_m_from :: Int -> Int -> Int -> [V] -> IO ()
+v_print_m_from k m n =
+    let k' = k * m
+        f = with_bars m . drop k' . v_dot_star (n * m + k')
+    in putStrLn . unlines . ("" :) . map f
