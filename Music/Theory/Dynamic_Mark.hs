730a269ef137418c829546aca69e8188498605c8
Rohan Drape <rd@slavepianos.org>
2012-06-10 10:30:59 +0000

    +Hairpin_T,implied_hairpin

--- Music/Theory/Dynamic_Mark.hs
+++ Music/Theory/Dynamic_Mark.hs
@@ -6,3 +6,16 @@ data Dynamic_Mark_T = Niente
                     | MF | F | FF | FFF | FFFF | FFFFF
                     | FP | SF | SFP | SFPP | SFZ | SFFZ
                       deriving (Eq,Ord,Enum,Bounded,Show)
+
+data Hairpin_T = Crescendo | Diminuendo | End_Hairpin
+                 deriving (Eq,Ord,Enum,Bounded,Show)
+
+-- | The 'Hairpin_T' implied by a sequence of 'Dynamic_Mark_T's.
+--
+-- > map (implied_hairpin MF) [MP,F] == [Just Diminuendo,Just Crescendo]
+implied_hairpin :: Dynamic_Mark_T -> Dynamic_Mark_T -> Maybe Hairpin_T
+implied_hairpin p q =
+    case compare p q of
+      LT -> Just Crescendo
+      EQ -> Nothing
+      GT -> Just Diminuendo
