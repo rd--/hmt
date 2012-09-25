04cfca39805259f0434fea7eeb2120156e2d97b9
Rohan Drape <rd@slavepianos.org>
2012-09-25 21:35:36 +0000

    +spell_cluster_c4_table(+verification!)

--- Music/Theory/Pitch/Spelling/Cluster.hs
+++ Music/Theory/Pitch/Spelling/Cluster.hs
@@ -5,8 +5,74 @@ import Data.List
 import Music.Theory.Pitch
 import Music.Theory.Pitch.Name
 
+-- | Spelling table for chromatic clusters.
+--
+-- > let f (p,q) = p == sort (map (snd . pitch_to_octpc) q)
+-- > in all f spell_cluster_c4_table == True
+spell_cluster_c4_table :: [([PitchClass],[Pitch])]
+spell_cluster_c4_table =
+    [([0],[c4])
+    ,([0,1],[c4,des4])
+    ,([0,1,2],[bis3,cis4,d4])
+    ,([0,1,2,11],[aisis3,bis3,cis4,d4])
+    ,([0,1,11],[b3,c4,des4])
+    ,([0,2],[c4,d4])
+    ,([0,2,11],[b3,c4,d4])
+    ,([0,11],[b3,c4])
+    ,([1],[cis4])
+    ,([1,2],[cis4,d4])
+    ,([1,2,11],[b3,cis4,d4])
+    ,([1,11],[b3,cis4])
+    ,([2],[d4])
+    ,([2,3],[d4,ees4])
+    ,([2,3,4],[d4,ees4,fes4])
+    ,([2,3,5],[d4,ees4,f4])
+    ,([2,3,4,5],[d4,ees4,fes4,geses4])
+    ,([2,4],[d4,e4])
+    ,([2,4,5],[d4,e4,f4])
+    ,([2,5],[d4,f4])
+    ,([2,11],[b3,d4])
+    ,([3],[ees4])
+    ,([3,4],[dis4,e4])
+    ,([3,4,5],[dis4,e4,f4])
+    ,([3,5],[ees4,f4])
+    ,([4],[e4])
+    ,([4,5],[e4,f4])
+    ,([5],[f4])
+    ,([5,6],[f4,ges4])
+    ,([5,6,7],[eis4,fis4,g4])
+    ,([5,6,8],[f4,ges4,aes4])
+    ,([5,6,9],[f4,ges4,a4])
+    ,([5,6,7,8],[eis4,fis4,g4,aes4])
+    ,([5,7],[f4,g4])
+    ,([5,7,8],[f4,g4,aes4])
+    ,([5,7,9],[f4,g4,a4])
+    ,([5,8],[f4,aes4])
+    ,([5,8,9],[f4,gis4,a4])
+    ,([5,9],[f4,a4])
+    ,([6],[fis4])
+    ,([6,7],[fis4,g4])
+    ,([6,7,8],[fis4,g4,aes4])
+    ,([6,7,9],[fis4,g4,a4])
+    ,([6,8],[fis4,gis4])
+    ,([6,8,9],[fis4,gis4,a4])
+    ,([6,9],[fis4,a4])
+    ,([7],[g4])
+    ,([7,8],[g4,aes4])
+    ,([7,8,9],[fisis4,gis4,a4])
+    ,([7,9],[g4,a4])
+    ,([8],[aes4])
+    ,([8,9],[gis4,a4])
+    ,([8,9,10],[gis4,a4,bes4])
+    ,([8,10],[aes4,bes4])
+    ,([9],[a4])
+    ,([9,10],[a4,bes4])
+    ,([10],[bes4])
+    ,([10,11],[ais4,b4])
+    ,([11],[b4])]
+
 -- | Spelling for chromatic clusters.  Sequence must be ascending.
--- Pitch class @0@ maps to 'c4', of there is no @0@ the all notes are
+-- Pitch class @0@ maps to 'c4', if there is no @0@ then all notes are
 -- in octave @4@.
 --
 -- > let f = fmap (map pitch_pp) . spell_cluster_c4
@@ -14,68 +80,7 @@ import Music.Theory.Pitch.Name
 --
 -- > fmap (map pitch_pp) (spell_cluster_c4 [10,11]) == Just ["A♯4","B4"]
 spell_cluster_c4 :: [PitchClass] -> Maybe [Pitch]
-spell_cluster_c4 p =
-    case sort p of
-      [0] -> Just [c4]
-      [0,1] -> Just [c4,des4]
-      [0,1,2] -> Just [bis3,cis4,d4]
-      [0,1,2,11] -> Just [aisis3,bis3,cis4,d4]
-      [0,1,11] -> Just [b3,c4,des4]
-      [0,2] -> Just [c4,d4]
-      [0,2,11] -> Just [b3,c4,d4]
-      [0,11] -> Just [b3,c4]
-      [1] -> Just [cis4]
-      [1,2] -> Just [cis4,d4]
-      [1,2,11] -> Just [b3,cis4,d4]
-      [1,11] -> Just [b3,cis4]
-      [2] -> Just [d4]
-      [2,3] -> Just [d4,ees4]
-      [2,3,4] -> Just [d4,ees4,fes4]
-      [2,3,5] -> Just [d4,ees4,f4]
-      [2,3,4,5] -> Just [d4,ees4,fes4,geses4]
-      [2,4] -> Just [d4,e4]
-      [2,4,5] -> Just [d4,e4,f4]
-      [2,5] -> Just [d4,f4]
-      [2,11] -> Just [b3,d4]
-      [3] -> Just [ees4]
-      [3,4] -> Just [dis4,e4]
-      [3,4,5] -> Just [dis4,e4,f4]
-      [3,5] -> Just [ees4,f4]
-      [4] -> Just [e4]
-      [4,5] -> Just [e4,f4]
-      [5] -> Just [f4]
-      [5,6] -> Just [f4,ges4]
-      [5,6,7] -> Just [eis4,fis4,g4]
-      [5,6,8] -> Just [f4,ges4,aes4]
-      [5,6,9] -> Just [f4,ges4,a4]
-      [5,6,7,8] -> Just [eis4,fis4,g4,aes4]
-      [5,7] -> Just [f4,g4]
-      [5,7,8] -> Just [f4,g4,aes4]
-      [5,7,9] -> Just [f4,g4,a4]
-      [5,8] -> Just [f4,aes4]
-      [5,8,9] -> Just [f4,gis4,a4]
-      [5,9] -> Just [f4,a4]
-      [6] -> Just [fis4]
-      [6,7] -> Just [fis4,g4]
-      [6,7,8] -> Just [fis4,g4,aes4]
-      [6,7,9] -> Just [fis4,g4,a4]
-      [6,8] -> Just [fis4,gis4]
-      [6,8,9] -> Just [fis4,gis4,a4]
-      [6,9] -> Just [fis4,a4]
-      [7] -> Just [g4]
-      [7,8] -> Just [g4,aes4]
-      [7,8,9] -> Just [fisis4,gis4,a4]
-      [7,9] -> Just [g4,a4]
-      [8] -> Just [aes4]
-      [8,9] -> Just [gis4,a4]
-      [8,9,10] -> Just [gis4,a4,bes4]
-      [8,10] -> Just [aes4,bes4]
-      [9] -> Just [a4]
-      [9,10] -> Just [a4,bes4]
-      [10] -> Just [bes4]
-      [10,11] -> Just [ais4,b4]
-      [11] -> Just [b4]
-      _ -> Nothing
+spell_cluster_c4 p = lookup (sort p) spell_cluster_c4_table
 
 -- | Variant of 'spell_cluster_c4' that runs 'pitch_edit_octave'.  An
 -- octave of @4@ is the identitiy, @3@ an octave below, @5@ an octave
