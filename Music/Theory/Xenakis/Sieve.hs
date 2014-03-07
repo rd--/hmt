7c47272e7a046be2f9d42f7e2469c3a95cea2ff0
Rohan Drape <rd@slavepianos.org>
2014-03-07 03:07:00 +0000

    ~Xenakis/Sieve(ex)

--- Music/Theory/Xenakis/Sieve.hs
+++ Music/Theory/Xenakis/Sieve.hs
@@ -3,7 +3,7 @@
 -- Vol. 28, No. 1 (Winter, 1990), pp. 58-78
 module Music.Theory.Xenakis.Sieve where
 
-import Data.List
+import qualified Data.List as L
 import Music.Theory.List
 
 -- | Synonym for 'Integer'
@@ -79,39 +79,52 @@ element s n =
 -- a sieve that contains an intersection clause that has no elements
 -- gives @_|_@.
 --
--- > let d = [0,2,4,5,7,9,11]
--- > in take 7 (build (union (map (l 12) d))) == d
+-- > let {d = [0,2,4,5,7,9,11]
+-- >     ;r = d ++ map (+ 12) d}
+-- > in take 14 (build (union (map (l 12) d))) == r
 build :: Sieve -> [I]
 build s =
-    let u_f = map head . group
+    let u_f = map head . L.group
         i_f = let g [x,_] = [x]
                   g _ = []
-              in concatMap g . group
+              in concatMap g . L.group
     in case s of
          Empty -> []
          L (m,i) -> [i, i+m ..]
          Union s0 s1 -> u_f (merge (build s0) (build s1))
          Intersection s0 s1 -> i_f (merge (build s0) (build s1))
 
--- | Variant of 'build' that gives the first /n/ places of the
--- 'reduce' of 'Sieve'.
---
--- > buildn 6 (union (map (l 8) [0,3,6])) == [0,3,6,8,11,14]
--- > buildn 12 (L (3,2)) == [2,5,8,11,14,17,20,23,26,29,32,35]
--- > buildn 9 (L (8,0)) == [0,8,16,24,32,40,48,56,64]
--- > buildn 3 (L (3,2) ∩ L (8,0)) == [8,32,56]
--- > buildn 12 (L (3,1) ∪ L (4,0)) == [0,1,4,7,8,10,12,13,16,19,20,22]
--- > buildn 14 (5⋄4 ∪ 3⋄2 ∪ 7⋄3) == [2,3,4,5,8,9,10,11,14,17,19,20,23,24]
--- > buildn 6 (3⋄0 ∪ 4⋄0) == [0,3,4,6,8,9]
--- > buildn 8 (5⋄2 ∩ 2⋄0 ∪ 7⋄3) == [2,3,10,12,17,22,24,31]
--- > buildn 12 (5⋄1 ∪ 7⋄2) == [1,2,6,9,11,16,21,23,26,30,31,36]
---
--- > buildn 10 (3⋄2 ∩ 4⋄7 ∪ 6⋄9 ∩ 15⋄18) == [3,11,23,33,35,47,59,63,71,83]
---
--- > let s = 3⋄2∩4⋄7∩6⋄11∩8⋄7 ∪ 6⋄9∩15⋄18 ∪ 13⋄5∩8⋄6∩4⋄2 ∪ 6⋄9∩15⋄19
--- > in buildn 16 s == buildn 16 (24⋄23 ∪ 30⋄3 ∪ 104⋄70)
---
--- > buildn 10 (24⋄23 ∪ 30⋄3 ∪ 104⋄70) == [3,23,33,47,63,70,71,93,95,119]
+{- | Variant of 'build' that gives the first /n/ places of the
+  'reduce' of 'Sieve'.
+
+> buildn 6 (union (map (l 8) [0,3,6])) == [0,3,6,8,11,14]
+> buildn 12 (L (3,2)) == [2,5,8,11,14,17,20,23,26,29,32,35]
+> buildn 9 (L (8,0)) == [0,8,16,24,32,40,48,56,64]
+> buildn 3 (L (3,2) ∩ L (8,0)) == [8,32,56]
+> buildn 12 (L (3,1) ∪ L (4,0)) == [0,1,4,7,8,10,12,13,16,19,20,22]
+> buildn 14 (5⋄4 ∪ 3⋄2 ∪ 7⋄3) == [2,3,4,5,8,9,10,11,14,17,19,20,23,24]
+> buildn 6 (3⋄0 ∪ 4⋄0) == [0,3,4,6,8,9]
+> buildn 8 (5⋄2 ∩ 2⋄0 ∪ 7⋄3) == [2,3,10,12,17,22,24,31]
+> buildn 12 (5⋄1 ∪ 7⋄2) == [1,2,6,9,11,16,21,23,26,30,31,36]
+
+> buildn 10 (3⋄2 ∩ 4⋄7 ∪ 6⋄9 ∩ 15⋄18) == [3,11,23,33,35,47,59,63,71,83]
+
+> let {s = 3⋄2∩4⋄7∩6⋄11∩8⋄7 ∪ 6⋄9∩15⋄18 ∪ 13⋄5∩8⋄6∩4⋄2 ∪ 6⋄9∩15⋄19
+>     ;s' = 24⋄23 ∪ 30⋄3 ∪ 104⋄70}
+> in buildn 16 s == buildn 16 s'
+
+> buildn 10 (24⋄23 ∪ 30⋄3 ∪ 104⋄70) == [3,23,33,47,63,70,71,93,95,119]
+
+> let r = [2,3,4,5,8,9,10,11,14,17,19,20,23,24,26,29,31]
+> in buildn 17 (5⋄4 ∪ 3⋄2 ∪ 7⋄3) == r
+
+> let r = [0,1,3,6,9,10,11,12,15,16,17,18,21,24,26,27,30]
+> in buildn 17 (5⋄1 ∪ 3⋄0 ∪ 7⋄3) == r
+
+> let r = [0,2,3,4,6,7,9,11,12,15,17,18,21,22,24,25,27,30,32]
+> in buildn 19 (5⋄2 ∪ 3⋄0 ∪ 7⋄4) == r
+
+-}
 buildn :: Int -> Sieve -> [I]
 buildn n = take n . build . reduce
 
