ee8191bf02aa7dce06a15d3408e34d8968176c9b
Rohan Drape <rd@slavepianos.org>
2014-03-08 03:12:39 +0000

    ~Time/Notation(+fsec_to_minsec,minsec_pp)

--- Music/Theory/Time/Notation.hs
+++ Music/Theory/Time/Notation.hs
@@ -5,8 +5,23 @@ import Text.Printf {- base -}
 -- | Fractional seconds.
 type FSEC = Double
 
+-- | Minutes, seconds as @(min,sec)@
+type MINSEC = (Int,Int)
+
 -- | Minutes, seconds, centi-seconds as @(min,sec,csec)@
 type MINCSEC = (Int,Int,Int)
+
+-- | Fractional seconds to @(min,sec)@.
+--
+-- > map fsec_to_minsec [59.49,60,60.51] == [(0,59),(1,0),(1,1)]
+fsec_to_minsec :: FSEC -> MINSEC
+fsec_to_minsec tm = round tm `divMod` 60
+
+-- | 'MINSEC' pretty printer.
+--
+-- > map (minsec_pp . fsec_to_minsec) [59,61] == ["00:59","01:01"]
+minsec_pp :: MINSEC -> String
+minsec_pp (m,s) = printf "%02d:%02d" m s
 
 -- | Fractional seconds to @(min,sec,csec)@.
 --
