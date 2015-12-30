0f495a2a7a1392ea1df2de93d02e954b98990b21
Rohan Drape <rd@slavepianos.org>
2015-12-30 03:18:47 +0000

    ~cmd/db (track...)

--- cmd/db.hs
+++ cmd/db.hs
@@ -6,19 +6,19 @@ import qualified Music.Theory.DB.JSON as JSON
 import qualified Music.Theory.DB.Plain as Plain
 
 db_load_ty :: String -> FilePath -> IO (Common.DB String String)
-db_load_ty ty =
+db_load_ty ty fn =
     case ty of
-      "plain" -> Plain.db_load_utf8
-      "json" -> JSON.db_load_utf8
-      "csv" -> CSV.db_load_utf8
+      "plain" -> fmap (map Common.record_uncollate) (Plain.db_load_utf8 Plain.sep_plain fn)
+      "json" -> JSON.db_load_utf8 fn
+      "csv" -> CSV.db_load_utf8 fn
       _ -> error "db_load_ty"
 
 db_store_ty :: String -> FilePath -> Common.DB String String -> IO ()
-db_store_ty ty =
+db_store_ty ty fn =
     case ty of
-      "plain" -> Plain.db_store_utf8
-      "json" -> JSON.db_store_utf8
-      "csv" -> CSV.db_store_utf8
+      "plain" -> Plain.db_store_utf8 Plain.sep_plain fn . map Common.record_collate
+      "json" -> JSON.db_store_utf8 fn
+      "csv" -> CSV.db_store_utf8 fn
       _ -> error "db_store_ty"
 
 -- > convert ("plain","csv") ("/home/rohan/in/CREDITS.txt","/tmp/t.csv")
@@ -37,5 +37,6 @@ main :: IO ()
 main = do
   a <- getArgs
   case a of
-    ["convert",input_ty,output_ty,input_fn,output_fn] -> convert (input_ty,output_ty) (input_fn,output_fn)
+    ["convert",input_ty,output_ty,input_fn,output_fn] ->
+         convert (input_ty,output_ty) (input_fn,output_fn)
     _ -> putStrLn (unlines help)
