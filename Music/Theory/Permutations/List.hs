81d9091cb41ee62dbf5d4093189d76dd57a11d1c
Rohan Drape <rd@slavepianos.org>
2016-01-05 06:12:06 +0000

    +multiset_permutations_n

--- Music/Theory/Permutations/List.hs
+++ Music/Theory/Permutations/List.hs
@@ -1,8 +1,10 @@
 -- | List permutation functions.
 module Music.Theory.Permutations.List where
 
-import qualified Math.Combinatorics.Multiset as C
-import qualified Music.Theory.Permutations as P
+import Data.List {- base -}
+import qualified Math.Combinatorics.Multiset as C {- multiset-comb -}
+
+import qualified Music.Theory.Permutations as P {- hmt -}
 
 -- | Generate all permutations.
 --
@@ -18,3 +20,20 @@ permutations i =
 -- > multiset_permutations [0,1,1] == [[0,1,1],[1,1,0],[1,0,1]]
 multiset_permutations :: (Ord a) => [a] -> [[a]]
 multiset_permutations = C.permutations . C.fromList
+
+factorial :: (Enum a, Num a) => a -> a
+factorial n = product [1..n]
+
+-- | Calculate number of permutations of a multiset.
+--
+-- > let r = factorial 11 `div` product (map factorial [1,4,4,2])
+-- > in multiset_permutations_n "MISSISSIPPI" == r
+--
+-- > multiset_permutations_n "MISSISSIPPI" == 34650
+-- > length (multiset_permutations "MISSISSIPPI") == 34650
+multiset_permutations_n :: Ord a => [a] -> Int
+multiset_permutations_n x =
+    let occ = map length . group . sort
+        n = factorial (length x)
+        d = product $ map factorial $ occ x
+    in n `div` d
