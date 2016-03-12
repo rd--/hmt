ad7a25e7b1dcdbf09e957268cd170ac85f23af01
Rohan Drape <rd@slavepianos.org>
2016-03-12 02:16:52 +0000

    ~cps_midi_tuning_f (+ex)

--- Music/Theory/Tuning.hs
+++ Music/Theory/Tuning.hs
@@ -393,6 +393,9 @@ d12_midi_tuning_f (t,c_diff,k) n =
 type CPS_Midi_Tuning = (Tuning,Double,Int,Int)
 
 -- | 'Midi_Tuning_F' for 'CPS_Midi_Tuning'.
+--
+-- > let f = cps_midi_tuning_f (equal_temperament 72,T.midi_to_cps 59,59,72 * 4)
+-- > map f [59 .. 131]
 cps_midi_tuning_f :: CPS_Midi_Tuning -> Midi_Tuning_F
 cps_midi_tuning_f (t,f0,k,g) n =
     let r = approximate_ratios_cyclic t
