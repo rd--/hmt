15222a9e26a1c826e8856d18dff6e0187ecb125d
Rohan Drape <rd@slavepianos.org>
2016-04-17 09:59:37 +0000

    +Music/Theory/Z/DK.hs

--- Music/Theory/Z.hs
+++ Music/Theory/Z.hs
@@ -1,27 +1,6 @@
 -- | Generalised Z-/n/ functions.
 module Music.Theory.Z where
 
-{-
-
-From GHC 7.6 onwards there is the modular-arithmetic package, which subsumes this work.
-
-{-# Language DataKinds #-}
-
-import Data.Modular {- modular-arithmetic -}
-import GHC.TypeLits {- base -}
-
-type Z n = Mod Integer n
-
--- > map negate [0::Z12 .. 11] == [0,11,10,9,8,7,6,5,4,3,2,1]
--- > map (+ 5) [0::Z12 .. 11] == [5,6,7,8,9,10,11,0,1,2,3,4]
-type Z12 = Mod Integer 12
-
--- > map invert [0::Z12 .. 11] == [0,11,10,9,8,7,6,5,4,3,2,1]
-invert :: KnownNat n => Z n -> Z n
-invert = negate
-
--}
-
 import Data.List {- base -}
 
 lift_unary_Z :: Integral a => a -> (t -> a) -> t -> a
