0c5177b864c3ccb094a92706ad4ec9ae1acf85d2
Rohan Drape <rd@slavepianos.org>
2016-04-26 00:44:55 +0000

    +rsg

--- Music/Theory/Z/Drape_1999.hs
+++ Music/Theory/Z/Drape_1999.hs
@@ -2,6 +2,7 @@ module Music.Theory.Z.Drape_1999 where
 
 import qualified Music.Theory.Set.List as T
 import Music.Theory.Z
+import Music.Theory.Z.SRO
 import Music.Theory.Z.TTO
 
 -- | Relate sets (TnMI).
@@ -13,3 +14,14 @@ rs m z x y =
     let xs = map (\o -> (o,z_tto_apply m z o x)) (z_tto_univ z)
         q = T.set y
     in filter (\(_,p) -> T.set p == q) xs
+
+{- | Relate segments.
+
+> rsg 5 mod12 [1,5,6] [3,11,10] == [sro_parse "T4I",sro_parse "r1RT4MI"]
+> rsg 5 mod12 [0,1,2,3] [0,5,10,3] == [sro_parse "T0M",sro_parse "RT3MI"]
+> rsg 5 mod12 [0,1,2,3] [4,11,6,1] == [sro_parse "T4MI",sro_parse "RT1M"]
+> rsg 5 mod12 [0,1,2,3] [11,6,1,4] == [sro_parse "r1T4MI",sro_parse "r1RT1M"]
+
+-}
+rsg :: Integral i => i -> Z i -> [i] -> [i] -> [SRO i]
+rsg m z x y = filter (\o -> z_sro_apply m z o x == y) (z_sro_univ (length x) z)
