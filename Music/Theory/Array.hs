342e8714a4e23697a36aff8ffc5b1933c558e567
Rohan Drape <rd@slavepianos.org>
2016-07-10 05:26:19 +0000

    ~Array/Matrix

--- Music/Theory/Array.hs
+++ Music/Theory/Array.hs
@@ -1,5 +1,6 @@
 module Music.Theory.Array where
 
+import Data.List {- base -}
 import qualified Data.Array as A {- array -}
 
 import qualified Music.Theory.List as T {- hmt -}
@@ -26,38 +27,56 @@ make_regular k tbl =
 -- | Matrix dimensions are written (rows,columns).
 type Dimensions i = (i,i)
 
--- | Matrix indices are written (row,column)
+-- | Matrix indices are written (row,column) & are here _zero_ indexed.
 type Ix i = (i,i)
 
--- | Translate Ix by row and column delta.
+-- | Translate 'Ix' by row and column delta.
+--
+-- > ix_translate (1,2) (3,4) == (4,6)
 ix_translate :: Num t => (t,t) -> Ix t -> Ix t
 ix_translate (dr,dc) (r,c) = (r + dr,c + dc)
 
--- | Modulo Ix by dimensions.
-ix_modulo :: Integral t => (t,t) -> Ix t -> Ix t
+-- | Modulo 'Ix' by 'Dimensions'.
+--
+-- > ix_modulo (4,4) (3,7) == (3,3)
+ix_modulo :: Integral t => Dimensions t -> Ix t -> Ix t
 ix_modulo (nr,nc) (r,c) = (r `mod` nr,c `mod` nc)
 
--- | All zero indexed matrix indices, in row order.
+-- > row_indices 3 1 == [(1,0),(1,1),(1,2)]
+row_indices :: (Enum t, Num t) => t -> t -> [Ix t]
+row_indices nc r = map (\c -> (r,c)) [0 .. nc - 1]
+
+-- > column_indices 2 1 == [(0,1),(1,1)]
+column_indices :: (Enum t, Num t) => t -> t -> [Ix t]
+column_indices nr c = map (\r -> (r,c)) [0 .. nr - 1]
+
+-- | All zero-indexed matrix indices, in row order.  This is the order
+-- given by 'sort'.
 --
 -- > matrix_indices (2,3) == [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2)]
+-- > sort (matrix_indices (2,3)) == matrix_indices (2,3)
 matrix_indices :: (Enum t, Num t) => Dimensions t -> [Ix t]
-matrix_indices (nr,nc) =
-    let f r = map (\c -> (r,c)) [0 .. nc - 1]
-    in concatMap f [0 .. nr - 1 ]
+matrix_indices (nr,nc) = concatMap (row_indices nc) [0 .. nr - 1 ]
 
--- | Rectangle corner indices in row order.
+-- | Corner indices of given 'Dimensions', in row order.
 --
--- > rectangle_corner_indices (2,3) == [(0,0),(0,2),(1,0),(1,2)]
-rectangle_corner_indices :: Num t => Dimensions t -> [Ix t]
-rectangle_corner_indices (nr,nc) = [(0,0),(0,nc - 1),(nr - 1,0),(nr - 1,nc - 1)]
+-- > matrix_corner_indices (2,3) == [(0,0),(0,2),(1,0),(1,2)]
+matrix_corner_indices :: Num t => Dimensions t -> [Ix t]
+matrix_corner_indices (nr,nc) = [(0,0),(0,nc - 1),(nr - 1,0),(nr - 1,nc - 1)]
 
--- | Trapezium given as dimensions with offset for lower indices.
+-- | Parallelogram corner indices, given as rectangular 'Dimensions' with an
+-- offset for the lower indices.
 --
--- > trapezium_corner_indices ((2,3),2) == [(0,0),(0,2),(1,2),(1,4)]
-trapezium_corner_indices :: Num t => (Dimensions t,t) -> [Ix t]
-trapezium_corner_indices ((nr,nc),o) = [(0,0),(0,nc - 1),(nr - 1,o),(nr - 1,nc + o - 1)]
+-- > parallelogram_corner_indices ((2,3),2) == [(0,0),(0,2),(1,2),(1,4)]
+parallelogram_corner_indices :: Num t => (Dimensions t,t) -> [Ix t]
+parallelogram_corner_indices ((nr,nc),o) = [(0,0),(0,nc - 1),(nr - 1,o),(nr - 1,nc + o - 1)]
 
+-- | Apply 'ix_modulo' and 'ix_translate' for all 'matrix_indices',
+-- ie. all translations of a 'shape' in row order.  The result may
+-- have duplicates.
+--
+-- > concat (all_ix_translations (2,3) [(0,0)]) == matrix_indices (2,3)
 all_ix_translations :: Integral t => Dimensions t -> [Ix t] -> [[Ix t]]
-all_ix_translations (nr,nc) ix =
-    let f z = ix_modulo (nr,nc) . ix_translate z
-    in [map (f (dr,dc)) ix | dr <- [0 .. nr - 1], dc <- [0 .. nc - 1]]
+all_ix_translations dm ix =
+    let f z = ix_modulo dm . ix_translate z
+    in map (\dx -> sort (map (f dx) ix)) (matrix_indices dm)
