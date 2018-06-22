e5c7f3061297be0e387ea44e5e5ebfc04176bc9e
Rohan Drape <rd@slavepianos.org>
2018-06-22 14:08:14 +0000

    ~mk/mk-convert,+abbrev

--- mk/mk-convert.hs
+++ mk/mk-convert.hs
@@ -7,17 +7,35 @@ gen_ts_conversion p q f =
     let nm = map toLower p ++ "_to_" ++ map toLower q
     in concat ["-- | Type specialised '",f,"'\n",nm," :: ",p," -> ",q,"\n",nm," = ",f,"\n"]
 
-int_types :: [String]
-int_types =
-    ["Word8","Word16","Word32","Word64"
-    ,"Int8","Int16","Int32","Int64"
-    ,"Int","Integer"]
+gen_ts_conversion_set :: String -> [String] -> [String] -> [String]
+gen_ts_conversion_set cnv p_ty q_ty =
+  [gen_ts_conversion p q cnv | p <- p_ty, q <- q_ty, p /= q]
+
+gen_type_abbvr :: (String, String) -> [String]
+gen_type_abbvr (p,q) = ["--| Alias",concat ["type ",q," = ",p]]
+
+-- > map gen_type_abbvr int_abbrev_table
+int_abbrev_table :: [(String,String)]
+int_abbrev_table =
+  zip
+  ["Word8","Word16","Word32","Word64","Int8","Int16","Int32","Int64","Int","Integer"]
+  ["U8","U16","U32","U64","I8","I16","I32","I64","I","L"]
+
+float_abbrev_table :: [(String,String)]
+float_abbrev_table = zip ["Float","Double"] ["F32","F64"]
 
 gen_int_conversions :: [String]
 gen_int_conversions =
-    let p_ty = int_types
-        q_ty = p_ty ++ ["Float","Double"]
-    in [gen_ts_conversion p q "fromIntegral" | p <- p_ty, q <- q_ty, p /= q]
+  let p = map fst int_abbrev_table
+      q = map fst float_abbrev_table
+  in gen_ts_conversion_set "fromIntegral" p (p ++ q)
+
+-- > mapM_ putStrLn gen_int_conversions_abbrev
+gen_int_conversions_abbrev :: [String]
+gen_int_conversions_abbrev =
+  let p = map snd int_abbrev_table
+      q = map snd float_abbrev_table
+  in gen_ts_conversion_set "fromIntegral" p (p ++ q)
 
 -- > putStrLn $ gen_ts_conversion_maybe "Word8" "Int" "fromIntegral"
 gen_ts_conversion_maybe :: String -> String -> String -> String
@@ -33,7 +51,7 @@ gen_ts_conversion_maybe p q f =
 
 gen_int_conversions_maybe :: [String]
 gen_int_conversions_maybe =
-    let p_ty = int_types
+    let p_ty = map fst int_abbrev_table
         q_ty = delete "Integer" p_ty
     in [gen_ts_conversion_maybe p q "fromIntegral" | p <- p_ty, q <- q_ty, p /= q]
 
