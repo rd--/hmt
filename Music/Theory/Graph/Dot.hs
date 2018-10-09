b25eab0257afa5903abb7282ae6915b7d02f92c1
Rohan Drape <rd@slavepianos.org>
2018-10-09 05:32:06 +0000

    ~dot_attr_def

--- Music/Theory/Graph/Dot.hs
+++ Music/Theory/Graph/Dot.hs
@@ -40,48 +40,58 @@ is_number = s_classify isDigit (\c -> isDigit c || c == '.') ((< 2) . length . f
 maybe_quote :: String -> String
 maybe_quote s = if is_symbol s || is_number s then s else concat ["\"",s,"\""]
 
--- * ATTR
+-- * ATTR/KEY
 
--- | area:opt (area = graph|node|edge)
-type DOT_KEY = String
 type DOT_OPT = String
 type DOT_VALUE = String
 type DOT_ATTR = (DOT_OPT,DOT_VALUE)
-type DOT_ATTR_SET = (String,[DOT_ATTR])
-
--- > dot_key_sep "graph:layout" == ("graph","layout")
-dot_key_sep :: DOT_KEY -> (String,String)
-dot_key_sep = T.split_on_1_err ":"
 
 dot_attr_pp :: DOT_ATTR -> String
 dot_attr_pp (lhs,rhs) = concat [lhs,"=",maybe_quote rhs]
 
+-- | Format sqeuence of DOT_ATTR.
+--
+-- > dot_attr_seq_pp [("layout","neato"),("epsilon","0.0001")]
 dot_attr_seq_pp :: [DOT_ATTR] -> String
 dot_attr_seq_pp opt =
   if null opt
   then ""
-  else concat [" [",intercalate "," (map dot_attr_pp opt),"]"]
+  else concat ["[",intercalate "," (map dot_attr_pp opt),"]"]
+
+dot_attr_ext :: [DOT_ATTR] -> [DOT_ATTR] -> [DOT_ATTR]
+dot_attr_ext = T.assoc_merge
+
+-- | (type,[attr])
+type DOT_ATTR_SET = (String,[DOT_ATTR])
 
 dot_attr_set_pp :: DOT_ATTR_SET -> String
 dot_attr_set_pp (ty,opt) = ty ++ dot_attr_seq_pp opt
 
-dot_attr_collate :: [DOT_ATTR] -> [DOT_ATTR_SET]
+-- | type:attr (type = graph|node|edge)
+type DOT_KEY = String
+type DOT_KV = (DOT_KEY,DOT_VALUE)
+
+-- | Keys are given as "type:attr".
+--
+-- > dot_key_sep "graph:layout" == ("graph","layout")
+dot_key_sep :: DOT_KEY -> (String,DOT_OPT)
+dot_key_sep = T.split_on_1_err ":"
+
+-- | Collate DOT_KEY attribute set to DOT_ATTR_SET.
+dot_attr_collate :: [DOT_KV] -> [DOT_ATTR_SET]
 dot_attr_collate opt =
     let f (k,v) = let (ty,nm) = dot_key_sep k in (ty,(nm,v))
         c = map f opt
     in T.collate c
 
-dot_attr_ext :: [DOT_ATTR] -> [DOT_ATTR] -> [DOT_ATTR]
-dot_attr_ext = T.assoc_merge
-
--- > map dot_attr_set_pp (dot_attr_collate dot_attr_def)
-dot_attr_def :: [DOT_ATTR]
-dot_attr_def =
-    [("graph:layout","neato")
-    ,("graph:epsilon","0.000001")
-    ,("node:fontsize","10")
-    ,("node:fontname","century schoolbook")
-    ,("node:shape","plaintext")]
+-- > k = dot_attr_def ("neato","century schoolbook",10,"plaintext")
+-- > map dot_attr_set_pp (dot_attr_collate k)
+dot_attr_def :: (String,String,Double,String) -> [(DOT_KEY,DOT_VALUE)]
+dot_attr_def (ly,fn,fs,sh) =
+    [("graph:layout",ly)
+    ,("node:fontname",fn)
+    ,("node:fontsize",show fs)
+    ,("node:shape",sh)]
 
 -- * GRAPH
 
@@ -131,24 +141,24 @@ type POS_FN v = (v -> (Int,Int))
 g_lift_pos_fn :: (v -> (Int,Int)) -> v -> [DOT_ATTR]
 g_lift_pos_fn f v = let (c,r) = f v in [("pos",show (c * 100) ++ "," ++ show (r * 100))]
 
-lve_to_dot :: G_TYPE -> [DOT_ATTR] -> GR_PP v e -> T.LVE v e -> [String]
+lve_to_dot :: G_TYPE -> [DOT_KV] -> GR_PP v e -> T.LVE v e -> [String]
 lve_to_dot g_typ opt (v_attr,e_attr) (v,e) =
     let v_f (k,lbl) = concat [show k,dot_attr_seq_pp (v_attr lbl),";"]
         e_f (lhs,rhs,lbl) = concat [show lhs,g_type_to_edge_symbol g_typ,show rhs
                                    ,dot_attr_seq_pp (e_attr lbl),";"]
     in concat [[g_type_to_string g_typ," g {"]
-              ,map dot_attr_set_pp (dot_attr_collate (T.assoc_merge opt dot_attr_def))
+              ,map dot_attr_set_pp (dot_attr_collate opt)
               ,map v_f v
               ,map e_f e
               ,["}"]]
 
-lve_to_udot :: [DOT_ATTR] -> GR_PP v e -> T.LVE v e -> [String]
+lve_to_udot :: [DOT_KV] -> GR_PP v e -> T.LVE v e -> [String]
 lve_to_udot o pp = lve_to_dot G_UGRAPH o pp
 
-fgl_to_dot :: G.Graph gr => G_TYPE -> [DOT_ATTR] -> GR_PP v e -> gr v e -> [String]
+fgl_to_dot :: G.Graph gr => G_TYPE -> [DOT_KV] -> GR_PP v e -> gr v e -> [String]
 fgl_to_dot typ opt pp gr = lve_to_dot typ opt pp (T.fgl_to_lve gr)
 
-fgl_to_udot :: G.Graph gr => [DOT_ATTR] -> GR_PP v e -> gr v e -> [String]
+fgl_to_udot :: G.Graph gr => [DOT_KV] -> GR_PP v e -> gr v e -> [String]
 fgl_to_udot opt pp gr = lve_to_udot opt pp (T.fgl_to_lve gr)
 
 -- * IO
