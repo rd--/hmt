2a5cc9fd891892a7d35410e8fde9c1e466b234d5
Rohan Drape <rd@slavepianos.org>
2018-12-19 05:11:21 +0000

    -> 0.18

--- Music/Theory/Monad.hs
+++ Music/Theory/Monad.hs
@@ -1,10 +1,12 @@
 -- | Monad functions.
 module Music.Theory.Monad where
 
-repeatM_ :: (Monad m) => m a -> m ()
+-- | 'sequence_' of 'repeat'.
+repeatM_ :: Monad m => m a -> m ()
 repeatM_ = sequence_ . repeat
 
-iterateM_ :: (Monad m) => st -> (st -> m st) -> m ()
-iterateM_ st f = do
+-- | Monadic variant of 'iterate'.
+iterateM_ :: Monad m => (st -> m st) -> st -> m ()
+iterateM_ f st = do
   st' <- f st
-  iterateM_ st' f
+  iterateM_ f st'
