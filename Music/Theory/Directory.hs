7bc8a655af507aec8b43f12bd4c481de8d495dd9
rd <rd@slavepianos.org>
2019-03-28 23:25:19 +0000

    +dir_find_ext

--- Music/Theory/Directory.hs
+++ Music/Theory/Directory.hs
@@ -5,8 +5,10 @@ import Data.List {- base -}
 import Data.Maybe {- base -}
 import System.Directory {- directory -}
 import System.FilePath {- filepath -}
+import System.Process {- process -}
 
 -- | Scan a list of directories until a file is located, or not.
+--   This does not traverse any sub-directory structure.
 path_scan :: [FilePath] -> FilePath -> IO (Maybe FilePath)
 path_scan p fn =
     case p of
@@ -15,13 +17,21 @@ path_scan p fn =
                     f x = if x then return (Just nm) else path_scan p' fn
                 in doesFileExist nm >>= f
 
+-- | Erroring variant.
 path_scan_err :: [FilePath] -> FilePath -> IO FilePath
 path_scan_err p x =
     let err = (error ("path_scan: " ++ show p ++ ": " ++ x))
     in fmap (fromMaybe err) (path_scan p x)
 
+-- | Find files having case-insensitive filename extension.
+--   This runs the system utility /find/, so is UNIX only.
+--
+-- > dir_find_ext ".syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+dir_find_ext :: String -> FilePath -> IO [FilePath]
+dir_find_ext ext dir = fmap lines (readProcess "find" [dir,"-iname",'*' : ext] "")
+
 -- | Subset of files in /dir/ with an extension in /ext/.
---   Extensions include the leading dot.
+--   Extensions include the leading dot and are case-sensitive.
 --
 -- > dir_subset [".hs"] "/home/rohan/sw/hmt/cmd"
 dir_subset :: [String] -> FilePath -> IO [FilePath]
