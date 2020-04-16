f73d1881b7592ea94504af1eb1805c61290eda36
rd <rd@slavepianos.org>
2020-04-16 03:00:56 +0000

    +split_when_keeping_left

--- Music/Theory/List.hs
+++ Music/Theory/List.hs
@@ -62,6 +62,12 @@ unbracket_err = fromMaybe (error "unbracket") . unbracket
 
 -- * Split
 
+-- | Variant of 'S.splitWhen' that keeps delimiters at left.
+--
+-- > split_when_keeping_left (== 'r') "rab rcd re rf r" == ["","rab ","rcd ","re ","rf ","r"]
+split_when_keeping_left :: (a -> Bool) -> [a] -> [[a]]
+split_when_keeping_left = S.split . S.keepDelimsL . S.whenElt
+
 -- | Relative of 'splitOn', but only makes first separation.
 --
 -- > splitOn "//" "lhs//rhs//rem" == ["lhs","rhs","rem"]
@@ -88,6 +94,8 @@ on_elem e = S.defaultSplitter { S.delimiter = S.Delimiter [(==) e] }
 --
 -- > map (flip split_before "abcde") "ae_" == [["","abcde"],["abcd","e"],["abcde"]]
 -- > map (flip break "abcde" . (==)) "ae_" == [("","abcde"),("abcd","e"),("abcde","")]
+--
+-- > split_before 'r' "rab rcd re rf r" == ["","rab ","rcd ","re ","rf ","r"]
 split_before :: Eq a => a -> [a] -> [[a]]
 split_before = S.split . S.keepDelimsL . on_elem
 
