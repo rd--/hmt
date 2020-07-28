c4db9d9c8034de4c346e39016789752adf2f570f
rd <rd@slavepianos.org>
2020-07-28 00:16:39 +0000

    +Graph/Planar

---
+++ Music/Theory/Graph/Planar.hs
@@ -0,0 +1,59 @@
+-- | <https://users.cecs.anu.edu.au/~bdm/plantri/plantri-guide.txt>
+module Music.Theory.Graph.Planar where
+
+import Data.List.Split {- split -}
+
+import qualified Data.ByteString as B {- bytestring -}
+
+import qualified Music.Theory.Graph.Type as T {- hmt -}
+
+plc_header_txt :: String
+plc_header_txt = ">>planar_code<<"
+
+plc_header :: B.ByteString -> String
+plc_header = map (toEnum . fromIntegral) . B.unpack . B.take 15
+
+plc_data :: B.ByteString -> [Int]
+plc_data = map fromIntegral . B.unpack . B.drop 15
+
+plc_length :: (Int,Int) -> Int
+plc_length (v,e) = v + 1 + 2 * e
+
+plc_scanner :: Int -> [Int] -> ([Int],[Int])
+plc_scanner =
+  let f r k i = case i of
+                  0:j -> if k == 1 then (reverse (0 : r),j) else f (0 : r) (k - 1) j
+                  e:j -> f (e : r) k j
+                  _ -> error "plc_scanner?"
+  in f []
+
+-- | (n-vertices,
+type PLC = (Int,[[Int]])
+
+plc_group :: Int -> [Int] -> PLC
+plc_group k i =
+  let c = endBy [0] i
+  in if length c == k then (k,c) else error "plc_group?"
+
+plc_segment :: [Int] -> [PLC]
+plc_segment i =
+  case i of
+    [] -> []
+    k:j -> case plc_scanner k j of
+             (r,[]) -> [plc_group k r]
+             (r,l) -> plc_group k r : plc_segment l
+
+plc_load :: FilePath -> IO [PLC]
+plc_load fn = do
+  b <- B.readFile fn
+  if plc_header b == plc_header_txt
+    then return (plc_segment (plc_data b))
+    else error "plc_load?"
+
+plc_to_g :: PLC -> T.G
+plc_to_g (k,n) =
+  let v = [0 .. k - 1]
+      f (i,j) = map (\x -> (i,x - 1)) j
+      g (i,j) = i <= j
+  in (v,filter g (concatMap f (zip v n)))
+
