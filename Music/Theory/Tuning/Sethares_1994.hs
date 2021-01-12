5137f4ad6f4c9ee9ff4ee6ccba263127cc8d253e
rd <rd@slavepianos.org>
2021-01-12 02:12:30 +0000

    ~Tuning.Sethares_1994,+clarity

--- Music/Theory/Tuning/Sethares_1994.hs
+++ Music/Theory/Tuning/Sethares_1994.hs
@@ -1,16 +1,26 @@
 -- | William A. Sethares.
 -- "Adaptive Tunings for Musical Scales".
 -- /Journal of the Acoustical Society of America/, 96(1), July 1994.
---
--- <http://sethares.engr.wisc.edu/consemi.html>
 module Music.Theory.Tuning.Sethares_1994 where
 
-import qualified Music.Theory.Tuning as T
+import Data.Maybe {- base -}
 
--- > import Sound.SC3.Plot
--- > plotTable1 (map (\f -> d (220,1) (f,1)) [220 .. 440])
-d :: (Floating n, Ord n) => (n,n) -> (n,n) -> n
-d (f1,v1) (f2,v2) =
+import qualified Music.Theory.Tuning as T {- hmt -}
+
+{- | Plomp-Levelt consonance curve.
+
+R. Plomp and W. J. M. Levelt,
+"Tonal Consonance and Critical Bandwidth,"
+Journal of the Acoustical Society of America.38, 548-560 (1965).
+
+"Relating Tuning and Timbre" <http://sethares.engr.wisc.edu/consemi.html>
+MATLAB: <https://sethares.engr.wisc.edu/comprog.html>
+
+> import Sound.SC3.Plot {- hsc3-plot -}
+> plot_p1_ln [map (\f -> pl_dissonance (220,1) (f,1)) [220 .. 440]]
+-}
+pl_dissonance :: (Floating n, Ord n) => (n,n) -> (n,n) -> n
+pl_dissonance (f1,v1) (f2,v2) =
     let d_star = 0.24
         s1 = 0.0207
         s2 = 18.96
@@ -24,16 +34,52 @@ d (f1,v1) (f2,v2) =
         e2 = c2 * exp (a2 * s * f_dif)
     in v1 * v2 * (e1 + e2)
 
--- > plotTable fig_1
-fig_1 :: (Floating n,Enum n,Ord n) => [[n]]
-fig_1 =
+-- | Sum of 'pl_dissonance' for all p in s1 and all q in s2.
+pl_dissonance_h :: (Floating n, Ord n) => [(n,n)] -> [(n,n)] -> n
+pl_dissonance_h s1 s2 = sum [pl_dissonance p q | p <- s1, q <- s2]
+
+-- | Return local minima of sequence with index.
+local_minima :: Ord t => [t] -> [(Int,t)]
+local_minima =
+  let f (ix,i,j,k) = if j <= i && j <= k then Just (ix,j) else Nothing
+      triples ix l = case l of
+                       i:j:k:_ -> (ix,i,j,k) : triples (ix + 1) (tail l)
+                       _ -> []
+  in mapMaybe f . triples 1
+
+-- | William A. Sethares "Adaptive Tunings for Musical Scales".
+--
+-- > plot_p1_ln atms_fig_1
+atms_fig_1 :: (Floating n,Enum n,Ord n) => [[n]]
+atms_fig_1 =
     let f0 = [125,250,500,1000,2000]
         r_seq = map T.cents_to_fratio [0 .. 1200]
-    in map (\f -> map (\r -> d (f,1) (f * r,1)) r_seq) f0
+    in map (\f -> map (\r -> pl_dissonance (f,1) (f * r,1)) r_seq) f0
 
--- > let a_seq = take 7 (iterate (* 0.88) 1.0)
--- > let gen f0 = zipWith (\r a -> (f0 * r,a)) [1 .. 7] a_seq
--- > let r_seq = map T.cents_to_ratio [0,1 .. 1200]
--- > plotTable1 (let f0 = 880 in map (\r -> d_h (gen f0) (gen (f0 * r))) r_seq)
-d_h :: (Floating n, Ord n) => [(n,n)] -> [(n,n)] -> n
-d_h s1 s2 = sum [d p q | p <- s1, q <- s2]
+-- > plot_p1_ln [atms_fig_2 880]
+-- > map fst (local_minima (atms_fig_2 880)) == [204,231,267,316,386,435,498,583,702,814,884,969,1018]
+atms_fig_2 :: (Ord t, Floating t, Enum t) => t -> [t]
+atms_fig_2 f0 =
+  let gen fq = map (\r -> (fq * r,1)) [1 .. 9]
+      r_seq = map T.cents_to_fratio [0,1 .. 1200]
+  in map (\r -> pl_dissonance_h (gen f0) (gen (f0 * r))) r_seq
+
+-- > plot_p1_ln [atms_fig_3 880]
+-- > map fst (local_minima (atms_fig_3 880)) == [267,400,533,667,800,933,1043]
+atms_fig_3 :: (Ord t, Floating t, Enum t) => t -> [t]
+atms_fig_3 f0 =
+  let b = 2 ** (1/9)
+      gen fq = map (\r -> (fq * r,1)) (1 : map (\n -> b ** n) [9,14,18,21,25,27,30])
+      r_seq = map T.cents_to_fratio [0,1 .. 1200]
+  in map (\r -> pl_dissonance_h (gen f0) (gen (f0 * r))) r_seq
+
+-- | "Relating Tuning and Timbre" <http://sethares.engr.wisc.edu/consemi.html>
+--
+-- > plot_p1_ln [rtt_fig_2 880]
+-- > map fst (local_minima (rtt_fig_2 880)) == [267,316,386,498,582,702,884,969]
+rtt_fig_2 :: (Ord t, Floating t, Enum t) => t -> [t]
+rtt_fig_2 f0 =
+  let a_seq = take 7 (iterate (* 0.88) 1.0)
+      gen fq = zipWith (\r a -> (fq * r,a)) [1 .. 7] a_seq
+      r_seq = map T.cents_to_fratio [0,1 .. 1200]
+  in map (\r -> pl_dissonance_h (gen f0) (gen (f0 * r))) r_seq
--- Music/Theory/Tuning/Syntonic.hs
+++ Music/Theory/Tuning/Syntonic.hs
@@ -3,11 +3,18 @@ module Music.Theory.Tuning.Syntonic where
 
 import Data.List {- base -}
 
-import Music.Theory.Tuning {- hmt -}
-import Music.Theory.Tuning.Type {- hmt -}
+import qualified Music.Theory.Tuning as T {- hmt -}
+import qualified Music.Theory.Tuning.Type as T {- hmt -}
 
--- | Construct an isomorphic layout of /r/ rows and /c/ columns with
--- an upper left value of /(i,j)/.
+{- | Construct an isomorphic layout of /r/ rows and /c/ columns with an upper left value of /(i,j)/.
+
+> r = [[(0,0),(-1,2),(-2,4)],[(-1,1),(-2,3),(-3,5)],[(-2,2),(-3,4),(-4,6)]]
+> mk_isomorphic_layout 3 3 (0,0) == r
+> map (map fst) r == [[0,-1,-2],[-1,-2,-3],[-2,-3,-4]]
+> map (map snd) r == [[0,2,4],[1,3,5],[2,4,6]]
+> map (map fst) r == map (map fst) (transpose r)
+> map (map snd) (transpose r) == [[0,1,2],[2,3,4],[4,5,6]]
+-}
 mk_isomorphic_layout :: Integral a => a -> a -> (a,a) -> [[(a,a)]]
 mk_isomorphic_layout n_row n_col top_left =
     let (a,b) `plus` (c,d) = (a+c,b+d)
@@ -22,9 +29,9 @@ mk_isomorphic_layout n_row n_col top_left =
 -- > [i,take 4 j,(2,-4):take 4 k] == minimal_isomorphic_note_layout
 minimal_isomorphic_note_layout :: [[(Int,Int)]]
 minimal_isomorphic_note_layout =
-    [[(3,-4),(2,-2),(1,0),(0,2),(-1,4)]
-       ,[(2,-3),(1,-1),(0,1),(-1,3)]
-    ,[(2,-4),(1,-2),(0,0),(-1,2),(-2,4)]]
+  [[(3,-4),(2,-2),(1,0),(0,2),(-1,4)]
+  ,[(2,-3),(1,-1),(0,1),(-1,3)]
+  ,[(2,-4),(1,-2),(0,0),(-1,2),(-2,4)]]
 
 -- | Make a rank two regular temperament from a list of /(i,j)/
 -- positions by applying the scalars /a/ and /b/.
@@ -35,7 +42,7 @@ rank_two_regular_temperament a b = let f (i,j) = i * a + j * b in map f
 -- rows and @7@ columns starting at @(3,-4)@ and a
 -- 'rank_two_regular_temperament' with /a/ of @1200@ and indicated
 -- /b/.
-mk_syntonic_tuning :: Int -> [Cents]
+mk_syntonic_tuning :: Int -> [T.Cents]
 mk_syntonic_tuning b =
   let l = mk_isomorphic_layout 5 7 (3,-4)
       t = map (rank_two_regular_temperament 1200 b) l
@@ -48,8 +55,8 @@ mk_syntonic_tuning b =
 > let c = [0,79,194,273,309,388,467,503,582,697,776,812,891,970,1006,1085,1164]
 > tn_cents_i syntonic_697 == c
 -}
-syntonic_697 :: Tuning
-syntonic_697 = Tuning (Right (mk_syntonic_tuning 697)) Nothing
+syntonic_697 :: T.Tuning
+syntonic_697 = T.Tuning (Right (mk_syntonic_tuning 697)) Nothing
 
 -- | 'mk_syntonic_tuning' of @702@.
 --
@@ -57,5 +64,5 @@ syntonic_697 = Tuning (Right (mk_syntonic_tuning 697)) Nothing
 --
 -- > let c = [0,24,114,204,294,318,408,498,522,612,702,792,816,906,996,1020,1110]
 -- > tn_cents_i syntonic_702 == c
-syntonic_702 :: Tuning
-syntonic_702 = Tuning (Right (mk_syntonic_tuning 702)) Nothing
+syntonic_702 :: T.Tuning
+syntonic_702 = T.Tuning (Right (mk_syntonic_tuning 702)) Nothing
