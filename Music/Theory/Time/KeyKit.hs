c1f008659e9305ba0f965119efca1f130e666ab9
rohan <rd@slavepianos.org>
2022-08-05 04:13:03 +1000

    +Music/Theory/Time/KeyKit/Basic.hs

--- Music/Theory/Time/KeyKit.hs
+++ Music/Theory/Time/KeyKit.hs
@@ -26,8 +26,14 @@ type Length = Time
 -- * Note
 
 data Note t =
-  Note { note_time :: Time, note_duration :: Duration, note_value :: t }
+  Note { note_start_time :: Time, note_duration :: Duration, note_value :: t }
   deriving (Eq, Ord, Show)
+
+note_end_time :: Note t -> Time
+note_end_time n = note_start_time n + note_duration n
+
+note_region :: Note t -> (Time, Time)
+note_region n = (note_start_time n, note_end_time n)
 
 note_shift_time :: Time -> Note t -> Note t
 note_shift_time k (Note t d e) = Note (t + k) d e
@@ -35,11 +41,14 @@ note_shift_time k (Note t d e) = Note (t + k) d e
 note_scale_duration :: Time -> Note t -> Note t
 note_scale_duration m (Note t d e) = Note t (d * m) e
 
-note_start_in_region :: (Time, Time) -> Note t -> Bool
-note_start_in_region (t1, t2) (Note t _ _) = t >= t1 && t < t2
+note_scale_duration_and_time :: Time -> Note t -> Note t
+note_scale_duration_and_time m (Note t d e) = Note (t * m) (d * m) e
 
-note_entirely_in_region :: (Time, Time) -> Note t -> Bool
-note_entirely_in_region (t1, t2) (Note t d _) = t >= t1 && (t + d) < t2
+note_is_start_in_region :: (Time, Time) -> Note t -> Bool
+note_is_start_in_region (t1, t2) (Note t _ _) = t >= t1 && t < t2
+
+note_is_entirely_in_region :: (Time, Time) -> Note t -> Bool
+note_is_entirely_in_region (t1, t2) (Note t d _) = t >= t1 && (t + d) < t2
 
 -- * Phrase
 
@@ -47,6 +56,9 @@ note_entirely_in_region (t1, t2) (Note t d _) = t >= t1 && (t + d) < t2
 data Phrase t =
   Phrase { phrase_notes :: [Note t], phrase_length :: Length }
   deriving (Eq, Ord, Show)
+
+phrase_values :: Phrase t -> [t]
+phrase_values = map note_value . phrase_notes
 
 phrase_set_length :: Phrase t -> Length -> Phrase t
 phrase_set_length (Phrase n _) l = Phrase n l
@@ -58,25 +70,31 @@ phrase_start_time :: Phrase t -> Time
 phrase_start_time (Phrase n _) =
   case n of
     [] -> 0
-    n1 : _ -> note_time n1
+    n1 : _ -> note_start_time n1
 
 phrase_end_time :: Phrase t -> Time
 phrase_end_time (Phrase n _) =
   case n of
     [] -> 0
-    _ -> note_time (last n)
+    _ -> note_start_time (last n)
 
 phrase_duration :: Phrase t -> Duration
 phrase_duration p = phrase_end_time p - phrase_start_time p
+
+phrase_maximum :: Ord t => Phrase t -> Note t
+phrase_maximum (Phrase n _) = maximum n
+
+phrase_minimum :: Ord t => Phrase t -> Note t
+phrase_minimum (Phrase n _) = minimum n
 
 -- | Keykit sets the length to the duration, i.e. ('c,e,g'%2).length is 192.
 phrase_at :: Phrase t -> Int -> Phrase t
 phrase_at (Phrase n _) k =
   let nt = n !! (k - 1)
-  in Phrase [nt] (note_time nt + note_duration nt)
+  in Phrase [nt] (note_start_time nt + note_duration nt)
 
 phrase_time_at :: Phrase t -> Int -> Time
-phrase_time_at (Phrase n _) k = note_time (n !! (k - 1))
+phrase_time_at (Phrase n _) k = note_start_time (n !! (k - 1))
 
 phrase_clear_at :: Phrase t -> Int -> Phrase t
 phrase_clear_at (Phrase n l) k =
@@ -87,14 +105,19 @@ phrase_at_put :: Ord t => Phrase t -> Int -> Phrase t -> Phrase t
 phrase_at_put (Phrase n1 l1) k (Phrase n2 _) =
   let nt = n1 !! (k - 1)
       remove_ix ix list = let (p,q) = splitAt ix list in p ++ tail q
-  in Phrase (O.merge (remove_ix (k - 1) n1) (map (note_shift_time (note_time nt)) n2)) l1
+  in Phrase (O.merge (remove_ix (k - 1) n1) (map (note_shift_time (note_start_time nt)) n2)) l1
 
 phrase_is_empty :: Phrase t -> Bool
 phrase_is_empty (Phrase n _) = null n
 
+-- | KeyKits p+q
 phrase_append :: Ord t => Phrase t -> Phrase t -> Phrase t
 phrase_append (Phrase n1 l1) (Phrase n2 l2) = Phrase (O.merge n1 (map (note_shift_time l1) n2)) (l1 + l2)
 
+phrase_append_list :: Ord t => [Phrase t] -> Phrase t
+phrase_append_list = foldl1' phrase_append
+
+-- | KeyKits p|q
 phrase_merge :: Ord t => Phrase t -> Phrase t -> Phrase t
 phrase_merge (Phrase n1 l1) (Phrase n2 l2) = Phrase (O.merge n1 n2) (max l1 l2)
 
@@ -113,19 +136,16 @@ phrase_partition (Phrase n l) f =
   in (Phrase n1 l, Phrase n2 l)
 
 phrase_select_region :: Phrase t -> (Time, Time) -> Phrase t
-phrase_select_region p r = phrase_select p (note_start_in_region r)
+phrase_select_region p r = phrase_select p (note_is_start_in_region r)
 
 phrase_clear_region :: Phrase t -> (Time, Time) -> Phrase t
-phrase_clear_region p r = phrase_select p (not . note_start_in_region r)
+phrase_clear_region p r = phrase_select p (not . note_is_start_in_region r)
 
 phrase_select_indices :: Phrase t -> (Int, Int) -> Phrase t
 phrase_select_indices (Phrase n l) (i, j) = Phrase (take (j - i + 1) (drop (i - 1) n)) l
 
 phrase_clear_indices :: Phrase t -> (Int, Int) -> Phrase t
 phrase_clear_indices (Phrase n l) (i, j) = Phrase (take (i - 1) n ++ drop j n) l
-
-phrase_shift :: Phrase t -> Time -> Phrase t
-phrase_shift (Phrase n l) t = Phrase (map (note_shift_time t) n) l
 
 phrase_extract_region :: Phrase t -> (Time, Time) -> Phrase t
 phrase_extract_region p (t1, t2) =
@@ -140,7 +160,7 @@ phrase_delete_region p (t1, t2) =
 
 phrase_separate :: Phrase t -> Time -> (Phrase t, Phrase t)
 phrase_separate p t =
-  let (p1, p2) = phrase_partition p (note_start_in_region (0, t))
+  let (p1, p2) = phrase_partition p (note_is_start_in_region (0, t))
       p1' = phrase_set_length p1 t
       p2' = phrase_set_length (phrase_shift p2 (0 - t)) (phrase_length p - t)
   in (p1', p2')
@@ -150,8 +170,8 @@ phrase_reverse (Phrase n l) =
   let f (Note t d e) = Note (l - t - d) d e
   in Phrase (reverse (map f n)) l
 
-phrase_shuffle :: Phrase t -> [Int] -> Phrase t
-phrase_shuffle (Phrase n l) p =
+phrase_reorder :: Phrase t -> [Int] -> Phrase t
+phrase_reorder (Phrase n l) p =
   let f (Note t d _) i = Note t d (note_value (n !! (i - 1)))
   in Phrase (zipWith f n p) l
 
@@ -182,6 +202,21 @@ phrase_phrase_map f (Phrase n l) =
 phrase_map :: Ord u => (Note t -> Phrase u) -> Phrase t -> Phrase u
 phrase_map f (Phrase n l) = Phrase (sort (concatMap phrase_notes (map f n))) l
 
+phrase_shift :: Phrase t -> Time -> Phrase t
+phrase_shift p t = phrase_note_map (note_shift_time t) p
+
+phrase_scale_duration :: Phrase t -> Time -> Phrase t
+phrase_scale_duration p m = phrase_note_map (note_scale_duration m) p
+
+phrase_scale_duration_and_time :: Phrase t -> Time -> Phrase t
+phrase_scale_duration_and_time p m = phrase_note_map (note_scale_duration_and_time m) p
+
+phrase_scale_to_duration :: Phrase t -> Duration -> Phrase t
+phrase_scale_to_duration p d = phrase_scale_duration_and_time p (d / phrase_length p)
+
+phrase_scale_to_region :: Phrase t -> (Time, Duration) -> Phrase t
+phrase_scale_to_region p (t1, t2) = phrase_shift (phrase_scale_to_duration p (t2 - t1)) t1
+
 -- * Seq
 
 phrase_to_wseq :: Phrase t -> Seq.Wseq Time t
@@ -199,4 +234,3 @@ wseq_to_phrase :: Seq.Wseq Time t -> Phrase t
 wseq_to_phrase sq =
   let f ((t, d), e) = Note t d e
   in Phrase (map f sq) (Seq.wseq_dur sq)
-
---
+++ Music/Theory/Time/KeyKit/Basic.hs
@@ -0,0 +1,52 @@
+-- | Translations of some functions from <https://github.com/nosuchtim/keykit/blob/master/lib/basic1.k>
+module Music.Theory.Time.KeyKit.Basic where
+
+import Data.List {- base -}
+
+import qualified Music.Theory.List as List {- hmt-base -}
+
+import Music.Theory.Time.KeyKit {- hmt -}
+
+{- | Returns an arpeggiated version of the phrase.
+One way of describing desc it is that all the notes have been separated and then put back together, back-to-back.
+
+> phrase_arpeggio (wseq_to_phrase (zip (repeat (0,1)) [60, 64, 67]))
+-}
+phrase_arpeggio :: Phrase t -> Phrase t
+phrase_arpeggio (Phrase n l) =
+  case n of
+    [] -> Phrase n l
+    n1 : _ ->
+      let t_seq = scanl (+) (note_start_time n1) (map note_duration n)
+          n' = zipWith (\t (Note _ d e) -> Note t d e) t_seq n
+          l' = note_end_time (last n)
+      in Phrase n' l'
+
+-- | Return phrase ph echoed num times, with rtime delay between each echo.
+phrase_echo :: Ord t => Phrase t -> Int -> Time -> Phrase t
+phrase_echo p n t = phrase_merge_list (map (\i -> phrase_shift p (fromIntegral i * t)) [0 .. n - 1])
+
+{- | Convert a phrase to be in step time, ie. all notes with the same spacing and duration.
+Overlapped notes (no matter how small the overlap) are played at the same time.
+
+> phrase_step (wseq_to_phrase [((0, 1), 60), ((5, 2), 64), ((23, 3), 67)]) 1
+-}
+phrase_step :: Phrase t -> Duration -> Phrase t
+phrase_step (Phrase n _) d =
+  let g = groupBy (\i j -> note_start_time i == note_start_time j) n
+      f l t = map (\(Note _ _ e) -> Note t d e) l
+      n' = concat (zipWith f g [0, d ..])
+  in Phrase n' (note_end_time (last n'))
+
+{- | This function takes a phrase, splits in in 2 halves (along time) and shuffles the result
+(ie. first a note from the first half, then a note from the second half, etc.).
+The timing of the original phrase is applied to the result.
+
+> phrase_to_wseq (phrase_shuffle (useq_to_phrase (1,[1..9])))
+-}
+phrase_shuffle :: Phrase t -> Phrase t
+phrase_shuffle (Phrase n l) =
+  let (lhs, rhs) = List.split_into_halves (map note_value n)
+      f (Note t d _) e = Note t d e
+      n' = zipWith f n (concat (transpose [lhs, rhs]))
+  in Phrase n' l
